806fce32f0619316a583007c51a138ddb1d63eea
0xpetra <franco.petraz@gmail.com>
2023-11-18 16:50:30 +0300

    🐛 FIX: test provided

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,19 +1,19 @@
 import '@/styles/globals.css'
 import { APP_NAME } from '@/lib/consts'
 import type { AppProps } from 'next/app'
-import { WagmiConfig, createClient } from 'wagmi'
+import { WagmiConfig, createClient, configureChains, mainnet } from 'wagmi'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
 import { ConnectKitProvider, getDefaultClient } from 'connectkit'
 
-const client = createClient(
-	getDefaultClient({
-		appName: APP_NAME,
-		infuraId: process.env.INFURA_ID,
-	})
-)
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+	[mainnet],
+	[alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()],
+  )
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<WagmiConfig client={client}>
+		<WagmiConfig client={publicClient}>
 			<ConnectKitProvider>
 				<Component {...pageProps} />
 			</ConnectKitProvider>
