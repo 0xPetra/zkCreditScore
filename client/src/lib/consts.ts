c807e0fa299a3e61ae48ba037e4fbf7a9c3082d6
0xpetra <franco.petraz@gmail.com>
2023-11-19 07:55:49 +0300

    🐛 FIX: config

--- client/src/lib/consts.ts
+++ client/src/lib/consts.ts
@@ -9,7 +9,6 @@ export const APP_NAME = "zk Credit Score";
     // To find the slot of a mapping, use the following command:
     // $ forge inspect <contract> storage-layout --pretty
     mappingSlot: ethers.toBeHex(300), // slot 300 of AxiomV2Query is `mapping(uint256 => AxiomQueryMetadata) public queries`
-    queryId:
-      66966676587627506390515603527468953886054461497745819784183473907733311885447n,
+    queryId: BigInt(66966676587627506390515603527468953886054461497745819784183473907733311885447),
     offset: 1, // offset 1 should be the `payment` field in `AxiomQueryMetadata`
   };
